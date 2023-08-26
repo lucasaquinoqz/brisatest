8e395ab512d4a4e1e829b4cbb6fa88894387cd4f
Lucas Aquino <lucasaquinoq@gmail.com>
2023-08-26 20:10:25 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+// Pegando a localozação do aparelho
+
+let local = {}
+
+function sucess(poss) {
+    local = { latitude: poss.coords.latitude, longitude: poss.coords.longitude }
+}
+
+if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(sucess);
+} else {
+    alert(
+        "Lamento, mas os serviços de geolocalização não são suportados pelo seu navegador.",
+    );
+}
+
+// ----------------------------------------------
+
+// Descobrindo se o dispositivo é mobile ou desktop
+function detectar_mobile() {
+    if (navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)
+    ) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(detectar_mobile())
+// -------------------------------------------
